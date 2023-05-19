e5c90531dbf2c5847137fa575abc57fed9939d75
Ankita <ankitatiwari40347@gmail.com>
2023-05-19 23:47:56 +0530

    msg

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ const modal = document.querySelector('.modal')
 const loginBtn = document.querySelector('.login-btn')
 const closeBtn = document.querySelector('.close')
 
-loginBtn.addEventListener('click',openModal)
-closeBtn.addEventListener('click',closeModal)
-window.addEventListener('click',outsideClick)
+loginBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
 
 function openModal() {
     modal.style.display = 'block'
